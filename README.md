a49c499b6fea0a5e78e3c5b40838afe56ad5ce4d
clemeno <clemeno@gmail.com>
2015-05-06 00:11:25 +0200

    minimal code for READ demo. from our ( insecure and single-node ) Cassandra `transactions` table in custom keyspace

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # SkimpyExpress
-Skimpy version of Express for NodeJS 
-to serve as a stable base framework for tiny server-side REST API projects. 
+Skimpy version of Express for NodeJS
+to serve as a stable base framework for tiny server-side REST API projects.
 
 **Will include some SQL and NoSQL database drivers later**
 
-CAP Theorem relative 
+## CAP Theorem relative additions
 
-## 2015-05-05 - Initial repo. with **Cassandra** driver 
+### 2015-05-05 - Initial repo. with **Cassandra** driver
 
-AP distributed column-oriented database with tunable C from heavy *FULL QUORUM* to softer local constraints. 
+AP distributed column-oriented database with tunable C from heavy *FULL QUORUM* to softer local constraints.
+
+https://www.npmjs.com/package/cassandra-driver
 
 
--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,46 @@
 var express = require( 'express' ),
+    cassandraDrv = require( 'cassandra-driver' ),
+    // use a default Cli to a fresh Cassandra demo. Test Cluster
+    // using custom arbitrary keyspace
+    cassandraCli = new cassandraDrv.Client( {
+      contactPoints: [ '127.0.0.1' ],
+      keyspace: 'terminaltransactionkeys001'
+    } ),
     router = express.Router()
-;
-router.route( '/rest/api/action' )
+; // custom REST route for our API
+router.route( '/rest/api/transaction' )
   .get( function( req, res, next ) {
-    res.send( {
-        method: 'GET',
-        route: '/rest/api/action'
+    // READ all data from a table `transactions`
+    cassandraCli.execute( ' SELECT * FROM transactions ', function( error, result ) {
+      // sending a consistant structure in this code
+      // but ( void 0 ) placeholders will disable irrelevant entries at runtime
+      if ( !error ) {
+        res.send( {
+          method: 'GET',
+          route: '/rest/api/transaction',
+          action: 'READ',
+          result: result,
+          error: void 0
+        } )
+      } else {
+        res.send( {
+          method: 'GET',
+          route: '/rest/api/transaction',
+          action: 'READ',
+          result: void 0,
+          error: error
+        } )
+      }
     } )
   } )
   .post( function( req, res, next ) {
+    // TODO
     res.send( {
-        method: 'POST',
-        route: '/rest/api/action'
+      method: 'POST',
+      route: '/rest/api/transaction',
+      action: 'CREATE',
+      result: void 0,
+      error: void 0
     } )
   } )
 ;
